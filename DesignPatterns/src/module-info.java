a48393ffa548bf989508bcef2472d3c05ba317f2
Saollin <gregory.janosz@gmail.com>
2020-04-25 18:06:27 +0200

    Add JavaFX and DesignPatterns.fxml

---
+++ DesignPatterns/src/module-info.java
@@ -0,0 +1,8 @@
+module DesignPatterns {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.desktop;
+
+    opens pl.agh.edu.dp.visualisation;
+    opens pl.agh.edu.dp.main;
+}
